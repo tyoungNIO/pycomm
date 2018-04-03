94508bd4ff71bb5f79ca5fc8254ff6f8377dd2dd
Tom Young <tyoung@n.io>
2018-04-03 15:41:58 -0600

    reorder class methods

--- pycomm/cip/cip_generic.py
+++ pycomm/cip/cip_generic.py
@@ -10,6 +10,26 @@ class Driver(Base):
         self._get_template_in_progress = False
         self.__version__ = '0.2'
 
+    def get_attribute_single(self, clss, inst, attr):
+        self.clear()
+        message_request = [
+            bytes([0x0E]),  # get attribute single service
+            bytes([0x03]),  # the Request Path Size length in word
+            bytes([0x20, clss, 0x24, inst, 0x30, attr]),  # the request path
+        ]
+        packet = build_common_packet_format(DATA_ITEM['Unconnected'],
+            b''.join(message_request),
+            ADDRESS_ITEM['UCMM'],)
+        if not self.send_rr_data(packet):
+            self._status = (6, "send_rr_data failed")
+            logger.warning(self._status)
+            raise DataError("send_rr_data failed")
+        if self._status[0] == SUCCESS:
+            # TODO: use packet to provide context for raw value(s)
+            return self._reply[-(self._reply[38] - 4):]
+        else:
+            return None
+
     def _check_reply(self):
         """ check the replayed message for error
 
@@ -41,22 +61,3 @@ class Driver(Base):
             return True
         except Exception as e:
             raise DataError(e)
-
-    def get_attribute_single(self, clss, inst, attr):
-        self.clear()
-        message_request = [
-            bytes([0x0E]),  # get attribute single service
-            bytes([0x03]),  # the Request Path Size length in word
-            bytes([0x20, clss, 0x24, inst, 0x30, attr]),  # the request path
-        ]
-        packet = build_common_packet_format(DATA_ITEM['Unconnected'],
-            b''.join(message_request),
-            ADDRESS_ITEM['UCMM'],)
-        if not self.send_rr_data(packet):
-            self._status = (6, "send_rr_data failed")
-            logger.warning(self._status)
-            raise DataError("send_rr_data failed")
-        if self._status[0] == SUCCESS:
-            return self._reply[-(self._reply[38] - 4):]
-        else:
-            return None
