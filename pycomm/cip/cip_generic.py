154ed27027a7f5cb29b60c2efe549805d2558415
Tom Young <tyoung@n.io>
2018-04-04 09:06:07 -0600

    add set_attr method

--- pycomm/cip/cip_generic.py
+++ pycomm/cip/cip_generic.py
@@ -10,12 +10,15 @@ class Driver(Base):
         self._get_template_in_progress = False
         self.__version__ = '0.2'
 
-    def get_attribute_single(self, clss, inst, attr):
+    def get_attribute_single(self, clss, inst, attr=None):
         self.clear()
+        path = [0x20, clss, 0x24, inst]
+        if attr != None:
+            path.extend([0x30, attr])
         message_request = [
             bytes([0x0E]),  # get attribute single service
             bytes([0x03]),  # the Request Path Size length in word
-            bytes([0x20, clss, 0x24, inst, 0x30, attr]),  # the request path
+            bytes(path),  # the request path
         ]
         packet = build_common_packet_format(
             DATA_ITEM['Unconnected'],
@@ -29,7 +32,31 @@ class Driver(Base):
             # TODO: use packet to provide context for raw value(s)
             return self._reply[-(self._reply[38] - 4):]
         else:
-            return None
+            return False
+
+    def set_attribute_single(self, data, clss, inst, attr=None):
+        self.clear()
+        path = [0x20, clss, 0x24, inst]
+        if attr != None:
+            path.extend([0x30, attr])
+        message_request = [
+            bytes([0x10]),  # get attribute single service
+            bytes([0x03]),  # the Request Path Size length in word
+            bytes(path),  # the request path
+            bytes(data),
+        ]
+        packet = build_common_packet_format(
+            DATA_ITEM['Unconnected'],
+            b''.join(message_request),
+            ADDRESS_ITEM['UCMM'],)
+        if not self.send_rr_data(packet):
+            self._status = (6, "send_rr_data failed")
+            logger.warning(self._status)
+            raise DataError("send_rr_data failed")
+        if self._status[0] == SUCCESS:
+            return True
+        else:
+            return False
 
     def _check_reply(self):
         """ check the reply message for error
